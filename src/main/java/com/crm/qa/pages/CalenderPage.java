a9611c00bdaf6a95ae972ae3d39b8974caaec45f
umesh <ukirpan311@gmail.com>
2021-09-15 22:38:12 +0530

    First Commit

---
+++ src/main/java/com/crm/qa/pages/CalenderPage.java
@@ -0,0 +1,28 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import com.crm.qa.base.TestBase;
+
+public class CalenderPage extends TestBase{
+
+	@FindBy(xpath="//*[@id='dashboard-toolbar']/div[1]")
+	WebElement CalenderPagetTitle;
+	
+	
+	@FindBy(xpath = "//span[contains(text(), 'Umesh Kirpan')]")
+	WebElement loggedinusername;
+	
+	//Initilization page objects
+	public CalenderPage() {
+		//PageFactory.initElements(driver, LoginPage.class);
+		PageFactory.initElements(driver, this);  
+	}
+	
+	//Actions
+	public String validateCalenderPageTitle() {
+		loggedinusername.click();
+		return CalenderPagetTitle.getText();
+	}
+}
